e1715d4d9ae915713ad5988d14b921f04df53276
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2024-10-12 09:42:51 +0300

    Update main.go Atoi

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+        "strconv"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	s, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+    s2 := ""
+	a := strings.Split(s, "")
+    for i:=0; i < len(a); i++{
+        var num, _ = strconv.Atoi(a[i])
+        s2 += strconv.Itoa(num * num)
+    }
+    fmt.Println(s2)
 }

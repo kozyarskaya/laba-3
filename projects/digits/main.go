49566762885cb9c44117ed84d3b5bf2da5b0a301
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2024-10-12 09:44:03 +0300

    Update main.go digits

--- projects/digits/main.go
+++ projects/digits/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+    "strconv"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	s, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	a := strings.Split(s, "")
+    maxi:=0
+    for i:=0; i  < len(a); i++{
+        var num, _ = strconv.Atoi(a[i])
+        if num > maxi{
+            maxi = num
+        }
+    }
+    fmt.Println(maxi)
 }
